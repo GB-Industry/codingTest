5d650655a611a86297b20814ed02cfd93af425ee
kgb <kkggbbpoint923@gmail.com>
2026-04-10 00:24:38 +0900

    260410 final test 디렉토리끼리 잘 묶이는지 최종 확인

---
+++ Baekjoon/Bj_2941/solution.java
@@ -0,0 +1,7 @@
+package Bj_2941; //크로아티아 알파벳
+
+public class solution {
+    public static void main(String[] args) {
+        System.out.println("DSf");
+    }
+}
--- Programmers/Pg_120816/solution.java
+++ Programmers/Pg_120816/solution.java
@@ -1,4 +1,4 @@
-package Pg_120816;
+package Pg_120816; //피자 나눠 먹기 (3)
 
 public class solution {
     public static void main(String[] args) {
