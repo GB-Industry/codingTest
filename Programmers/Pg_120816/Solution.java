1b8ec0d91057bbf6a6610a1cbe23b907134fe2b9
kgb <kkggbbpoint923@gmail.com>
2026-04-14 22:12:49 +0900

    solve pg_120816 피자 나눠 먹기(3)

--- Programmers/Pg_120816/Solution.java
+++ Programmers/Pg_120816/Solution.java
@@ -2,6 +2,23 @@ package Pg_120816; //피자 나눠 먹기 (3)
 
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(solution(7,10));
+        System.out.println(solution(4,12));
 
+    }
+    public static int solution(int slice, int n) {
+        int answer;
+
+        if (slice > n){
+            answer = 1;
+        }else{
+            if (n%slice == 0){ //
+                answer = n/slice;
+            }else{
+                answer = n/slice + 1;
+            }
+        }
+
+        return answer;
     }
 }
